32ca1ea5c4accdee0d84373b3566327f987601cc
Jdsatashi <79429478+Jdsatashi@users.noreply.github.com>
2021-03-04 16:20:45 +0700

    Update product.php

--- product.php
+++ product.php
@@ -10,24 +10,14 @@
 </head>
 
 <body>
-	
-	<ul id="mypetlist" data-role="listview">
-		<li><a>This is an item 1</a></li>
-		<li><a>This is an item 2</a></li>
-	</ul>
+	<form method="popst" action="productscipt.php">
+	Id: <input type="text" name="id" id="id"><br>
+	Product name: <input type="text" name="productname" id="productname"><br>
+	Category: <input type="text" name="category" id="category"><br>
+	Price: <input type="text" name="price" id="price"><br>
+	Description: <input type="text" name="Description" id="Description"><br>
+	<input type="summit">
+</form>
 
-	<a id="btnparrot" href="" class="ui-btn">Add a Parrot</a>
-	<button class="ui-btn ui-icon-delete ui-btn-icon-left">Add a Dog</button>
-
-	<script>
-		//$(document): equal to Javascript document.
-		//$(document).ready(...) called when this document is loaded
-		$("#btnparrot").click(addParrot); //equivalent to Javascript document.getElementById("btnparrot").onclick = ...;
-
-		function addParrot(){
-			$("#mypetlist").append("<li><a>a Dog</a><button>View</button></li>").listview("refresh");
-			
-		}
-	</script>
 </body>
 </html>
