29a82daef3fc21770d2afd5de6291b20e647d459
Jdsatashi <79429478+Jdsatashi@users.noreply.github.com>
2021-03-05 16:24:00 +0700

    Create test.php

---
+++ test.php
@@ -0,0 +1,54 @@
+<html>
+<body>
+<h2>You have summitted the following to the server: </h2>
+<?php
+/* Attempt Heroku Postgres connection 
+	Assuming you are running Heroku Postgres add-on
+	with default setting*/
+    $host = "ec2-34-201-248-246.compute-1.amazonaws.com";
+    $dbname = "da79i3d6vat4tl";
+    $port = "5432";
+    $user = "tapdzjqevixblx";
+    $pass = "adbd7cd24cae262eaf82c03725969b97984d969afbd7cac9637f9008bcc4f8ec";
+    $ssl = "require";
+
+    $link = pg_connect("host=".$host." dbname=".$dbname." port=".$port." user=".$user." password=".$pass." sslmode=".$ssl); 
+	// Check connection
+	if($link === false){
+		die("ERROR: Could not connect.");
+	} else {
+		echo "Connection to Heroku Postgres has been established";
+	}
+	
+	 
+	// Escape user inputs for security
+	//$id = mysqli_real_escape_string($link, $_REQUEST['pid']);
+	//$name = mysqli_real_escape_string($link, $_REQUEST['pname']);
+	//$cate = mysqli_real_escape_string($link, $_REQUEST['pcate']);
+	//$cost = mysqli_real_escape_string($link, $_REQUEST['pcost']);
+
+
+	$id = "003";
+	$name = "Product";
+	$cate = "Default";
+	$cost = "100";
+	 
+	// Attempt insert query execution
+	$sql4 = 'INSERT INTO public."Product" (
+			"pid","pname","pcate","pcost") VALUES ('."
+			'$id'::character varying,'$name'::character varying,'$cate'::character varying,'$cost'::integer)".
+			 'returning "id"';
+	
+	//$sql = "INSERT INTO public.Product (pid, pname, pcate, pcost) VALUES			('002','My Product','Default11','10080')";
+	
+	if(pg_query($link, $sql4)){
+		echo "Records added successfully.";
+	} else{
+		echo "ERROR: Could not able to execute $sql. " . pg_error($link);
+	}
+	
+	// Close connection
+	pg_close($link);
+?>
+</body>
+</html>
