ab1484e3900e3314be6530c8dde1744f135f80c4
Jdsatashi <79429478+Jdsatashi@users.noreply.github.com>
2021-03-05 16:03:05 +0700

    Update addproduct.php

--- addproduct.php
+++ addproduct.php
@@ -39,7 +39,7 @@
 			'$id'::character varying,'$name'::character varying,'$cate'::character varying,'$cost'::integer)".
 			 'returning "id"';
 */
-    $sql4 = "INSERT INTO public.'Product'(pid, pname, pcate, pcost) VALUES ('$id', '$name', '$cate', '$cost')";
+    $sql4 = 'INSERT INTO public."Product"(pid, pname, pcate, pcost) VALUES ("$id", "$name", "$cate", "$cost")';
     echo $sql4;
     
     if(pg_query($link, $sql4)){
