4a8e06b9a718d27ce9547a777bc762c0135a6774
Jdsatashi <79429478+Jdsatashi@users.noreply.github.com>
2021-03-05 20:38:26 +0700

    Update addproduct.php

--- addproduct.php
+++ addproduct.php
@@ -33,7 +33,7 @@
 */
 
 
-$sql = 'INSERT INTO "Product"(pid,pname,pcate,pcost) VALUES ('."'$id'::character varying,'$name'::character varying,'$cate'::character varying,'$cost'::integer)".'returning "id"';
+$sql = 'INSERT INTO "Product" VALUES ('."'$id'::character varying,'$name'::character varying,'$cate'::character varying,'$cost'::integer)".'returning "id"';
 
     //$mysqlquery = "INSERT INTO Product(pid, pname, pcate, pcost) VALUES ('$id', '$name', '$cate', '$cost')";
     
