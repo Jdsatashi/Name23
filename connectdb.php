acea5bf990e971846b02aaa41bef2dffec4f8f36
Jdsatashi <79429478+Jdsatashi@users.noreply.github.com>
2021-03-05 13:04:26 +0700

    Update connectdb.php

--- connectdb.php
+++ connectdb.php
@@ -6,50 +6,50 @@
 <p> Price:     <?php echo $_POST["price"]; ?> <br>
 
 <?php
-$host = "ec2-34-201-248-246.compute-1.amazonaws.com";
-$dbname = "da79i3d6vat4tl";
-$port = "5432";
-$user = "tapdzjqevixblx";
-$pass = "adbd7cd24cae262eaf82c03725969b97984d969afbd7cac9637f9008bcc4f8ec";
-$ssl = "require";
-
-$link = pg_connect("host=".$host." dbname=".$dbname." port=".$port." user=".$user." password=".$pass." sslmode".ssl);
+/* Attempt Heroku Postgres connection 
+	Assuming you are running Heroku Postgres add-on
+	with default setting*/
+	$link = pg_connect("host=ec2-34-201-248-246.compute-1.amazonaws.com dbname=da79i3d6vat4tl port=5432 user=tapdzjqevixblx password=adbd7cd24cae262eaf82c03725969b97984d969afbd7cac9637f9008bcc4f8ec sslmode=require");
+	 
+	// Check connection
 	if($link === false){
 		die("ERROR: Could not connect.");
 	} else {
 		echo "Connection to Heroku Postgres has been established";
 	}
-$id = mysql_real_escape_string($link, $_REQUEST['id']);
-
-	$id= $_REQUEST['id'];
-	$name = $_REQUEST['name'];
-	$cat = $_REQUEST['category'];
-	$price = $_REQUEST['price'];
 	
+	 
+	// Escape user inputs for security
 	//$id = mysqli_real_escape_string($link, $_REQUEST['id']);
 	//$name = mysqli_real_escape_string($link, $_REQUEST['name']);
 	//$cat = mysqli_real_escape_string($link, $_REQUEST['cat']);
+	//$date = mysqli_real_escape_string($link, $_REQUEST['date']);
 	//$price = mysqli_real_escape_string($link, $_REQUEST['price']);
-	
-	echo $id;
-	echo $name;
-	echo $cat;
-	echo $price;
-	
-	//$sql4 = "INSERT INTO Product (id, name, category, price) VALUES ('$id', '$name', '$cat', '$price')";
+	//$description = mysqli_real_escape_string($link, $_REQUEST['description']);
 
-	$sql4 = 'INSERT INTO public."Product" ("id", "name", "category", "price",) VALUES ('."'$id': :character varying,
-	'$name': :character varying, '$cat': :character varying, '$price': :integer)".
-	' returning "id"';
+	$id = "001";
+	$name = "My Product";
+	$cat = "Default";
+	$date = "04-24-2020";
+	$price = "100";
+	$desc = "Default";
+	 
+	// Attempt insert query execution
+	$sql4 = 'INSERT INTO public."Product" (
+			"id","product_name","category","date","price","descriptions") VALUES ('."
+			'$id'::character varying,'$name'::character varying,'$cat'::character varying,'$date'::date,'$price'::integer,'$desc'::character varying)".
+			 'returning "id"';
 	
-	echo $sql4;
+	//$sql = "INSERT INTO public.Product (id, product_name, category, date, price, descriptions) VALUES			('001','My Product','Default','04/24/2020','100','Default')";
 	
 	if(pg_query($link, $sql4)){
 		echo "Records added successfully.";
-	} else {
-		echo "ERROR: Could not able to execute $sql4. " . pg_error($link);
+	} else{
+		echo "ERROR: Could not able to execute $sql. " . pg_error($link);
 	}
-		pg_close($link);
+	
+	// Close connection
+	pg_close($link);
 ?>
 </body>
 </html>
