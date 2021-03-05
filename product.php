35c816a39e7230d11a144bcd342f7109b5177d7f
Jdsatashi <79429478+Jdsatashi@users.noreply.github.com>
2021-03-05 10:13:58 +0700

    Update product.php

--- product.php
+++ product.php
@@ -7,14 +7,87 @@
 	<link rel="stylesheet" href="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.css" />
 	<script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
 	<script src="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
+<style>
+* {
+  box-sizing: border-box;
+}
+
+input[type=text], select, textarea {
+  width: 100%;
+  padding: 12px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  resize: vertical;
+}
+
+label {
+  padding: 12px 12px 12px 0;
+  display: inline-block;
+}
+
+input[type=submit] {
+  background-color: #4CAF50;
+  color: white;
+  padding: 12px 20px;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+  float: right;
+}
+
+input[type=submit]:hover {
+  background-color: #45a049;
+}
+
+.container {
+  border-radius: 5px;
+  background-color: #f2f2f2;
+  padding: 20px;
+}
+
+.col-25 {
+  float: left;
+  width: 25%;
+  margin-top: 6px;
+}
+
+.col-75 {
+  float: left;
+  width: 75%;
+  margin-top: 6px;
+}
+
+/* Clear floats after the columns */
+.row:after {
+  content: "";
+  display: table;
+  clear: both;
+}
+
+/* Responsive layout - when the screen is less than 600px wide, make the two columns stack on top of each other instead of next to each other */
+@media screen and (max-width: 600px) {
+  .col-25, .col-75, input[type=submit] {
+    width: 100%;
+    margin-top: 0;
+  }
+}
+</style>
 </head>
 
 <body>
 	<form method="post" action="connectdb.php">
-	Id: <input type="text" name="id" id="id"><br>
 	Product name: <input type="text" name="name" id="name"><br>
 	Category: <input type="text" name="category" id="category"><br>
 	Price: <input type="text" name="price" id="price"><br>
+	<input type="hidden" id="controlUpdate" name="controlUpdate" value="<?php echo $isUpdated?>" />
+  <div class="row">
+    <div class="col-25">
+      <label for="name">Product Id</label>
+    </div>
+    <div class="col-75">
+      <input type="text" id="id" name="id" value="<?php echo $id?>" <?php if ($isUpdated == 1) echo "readonly";?> placeholder="product id..">
+    </div>	
+  </div>
 	<input type="submit">
 </form>
 
