5068bf2aa8bed07377850c1f04b73171e954af4e
Jdsatashi <79429478+Jdsatashi@users.noreply.github.com>
2021-03-09 17:37:25 +0700

    Update Web.php

--- Web.php
+++ Web.php
@@ -273,11 +273,16 @@ body {
     $prod = pg_query($link, $query);
 	?>
 	<?php   
-while ($row = pg_fetch_row($prod)) {?>
-<h3>yeah  <?php echo "$row['pid'] $row['pname'] ";?></h3>
-<?php }
+while ($row = pg_fetch_row($prod)) { ?>
 
-pg_close($con); ?>
+<img src="<?php echo "$row['pimg']";?>">
+<h3><?php echo "$row['pname']";?></h3>
+<h6>Super toys <?php echo "$row['pname']";?> was made in Vietnam or some where.</h6>
+<h6><a href="detail.php?pid=<?php echo "$row['pid'"?>"><button type="button" class="btn btn-info">Buy</button></a></h6>
+
+<?php
+pg_close($link); 
+} ?>
 	
 
 <h3 class="bg-primary">What toys you want owned? Let's searching toys you like most.</h3>
