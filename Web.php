5bbab00d39b60af9445df710416bd7332d3bf727
Jdsatashi <79429478+Jdsatashi@users.noreply.github.com>
2021-03-10 10:37:54 +0700

    Update Web.php

--- Web.php
+++ Web.php
@@ -6,7 +6,7 @@
 		<title>
 			Figure Shop JDsatashi
 		</title>
-<link rel="stylesheet" href="bootstrap/bootstrap.min.css" integrity="crossorigin="anonymous">
+<link rel="stylesheet" href="bootstrap/bootstrap.min.css" crossorigin="anonymous">
 
 <link rel="stylesheet" href="bootstrap/bootstrap-theme.min.css"crossorigin="anonymous">
 
