bdb171aacf2791b95208ffd6a9139b25fadd5124
Jdsatashi <79429478+Jdsatashi@users.noreply.github.com>
2021-03-11 19:26:54 +0700

    Update addinvoice.php

--- addinvoice.php
+++ addinvoice.php
@@ -68,7 +68,7 @@ $sql = 'INSERT INTO "invoice"("pid","cid","price","datebuy") VALUES ('."
 <th>Date buy</th>
 </tr>
 <tr>
-<td><?phpwhile ($row = pg_fetch_assoc($prod)) { $row['invid'] };?></td>
+<td><?php while ($row = pg_fetch_assoc($prod)) { $row['invid']; }?></td>
 <td><?php echo $pid ?></td>
 <td><?php echo $cid ?></td>
 <td><?php echo $price ?></td>
