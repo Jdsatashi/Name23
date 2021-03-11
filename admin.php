2ab3f8d56d89278addbc944648c9e0e5129c329c
Jdsatashi <79429478+Jdsatashi@users.noreply.github.com>
2021-03-11 17:26:58 +0700

    Update admin.php

--- admin.php
+++ admin.php
@@ -4,6 +4,6 @@
 <body>
 <h1> Admin Page </h1>
 <h3><a href="insertpro.php"> Manage Product</a></h3>
-<h3><a href="insertcus.php"> Manage Product</a></h3>
+<h3><a href="insertcus.php"> Manage Customer</a></h3>
 </body>
 </html>
