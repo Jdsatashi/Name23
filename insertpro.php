7893d6e63330083006ebf34b823d6217a6824057
Jdsatashi <79429478+Jdsatashi@users.noreply.github.com>
2021-03-14 20:23:05 +0700

    Update insertpro.php

--- insertpro.php
+++ insertpro.php
@@ -1,12 +1,7 @@
 <html>
 <head>
 	<title>JQuery Mobile Lab 2</title>
-
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-
-	<link rel="stylesheet" href="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.css" />
-	<script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
-	<script src="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
 <style>
 * {
   box-sizing: border-box;
@@ -46,6 +41,5 @@ Type price:		<input type="text" name="pcost" id="pcost"><br>
 Type name image of product: <input type="text" name="pimg" id="pimg"><br>
 	<input type="submit">
 </form>
-
 </body>
 </html>
